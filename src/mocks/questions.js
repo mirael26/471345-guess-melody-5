829dddde152770d482623a6cf7cfcf3ee132250b
Эльмира <capricorn_26@mail.ru>
2020-10-19 23:15:09 +0700

    Поправляет моки

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -7,17 +7,21 @@ export default [
     answers: [{
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `rock`,
-    }, {
+    },
+    {
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `blues`,
-    }, {
+    },
+    {
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `jazz`,
-    }, {
+    },
+    {
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `rock`,
     }]
-  }, {
+  },
+  {
     type: `artist`,
     song: {
       artist: `Jim Beam`,
@@ -26,10 +30,12 @@ export default [
     answers: [{
       picture: `${AVATAR_URL}/${Math.random()}`,
       artist: `John Snow`,
-    }, {
+    },
+    {
       picture: `${AVATAR_URL}/${Math.random()}`,
       artist: `Jack Daniels`,
-    }, {
+    },
+    {
       picture: `${AVATAR_URL}/${Math.random()}`,
       artist: `Jim Beam`,
     }],
